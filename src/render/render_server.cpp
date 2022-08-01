64426a0f2726279e322915f36a4192bf465cc0f1
floppyhammer <tannhauser_chen@outlook.com>
2022-08-01 19:57:59 +0800

    Add comments related to depth compare op for skybox

--- src/render/render_server.cpp
+++ src/render/render_server.cpp
@@ -1380,6 +1380,7 @@ namespace Flint {
         depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
         depthStencil.depthTestEnable = VK_TRUE;
         depthStencil.depthWriteEnable = VK_TRUE;
+        // This has to be LESS_OR_EQUAL for correct skybox rendering.
         depthStencil.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
         depthStencil.depthBoundsTestEnable = VK_FALSE;
         depthStencil.minDepthBounds = 0.0f; // Optional
