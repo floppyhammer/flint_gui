2444829569fdba50fb416d50307272bb84526847
floppyhammer <tannhauser_chen@outlook.com>
2025-02-25 11:41:43 +0800

    Include input_server.h in app.h

--- src/app.h
+++ src/app.h
@@ -12,6 +12,7 @@
 #include "common/geometry.h"
 #include "nodes/scene_tree.h"
 #include "render/blit.h"
+#include "servers/input_server.h"
 #include "servers/translation_server.h"
 
 namespace Flint {
